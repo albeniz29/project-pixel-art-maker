5c2a2782e4f06a115c51af43cf9004c2eef92488
albeniz29 <49162136+albeniz29@users.noreply.github.com>
2019-05-05 09:48:40 -0400

    removing var and using const and let

--- designs.js
+++ designs.js
@@ -9,47 +9,42 @@ function makeGrid(x, y) {
    $('tr').remove();
 
 // loop to insert the rows
-   for (var i = 1; i<= x; i++) {
+   for (let i = 1; i<= x; i++) {
      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
 
   // loop to insert the columns
-     for (var z = 1; z <= y; z++) {
+     for (let z = 1; z <= y; z++) {
        $('#table' + i).append('<td></td>');
      }
     }
 
 // this function lets the user select the color and change it if needed
-
 /* Choosing a color from the color selector and clicking on a grid square
 causes that grid square (and only that square) to change color.
 */
+// adding the click event to the color picked
+  $('td').click(function addColor(){
+    // storing the color picked
+    const color = $('#colorPicker').val();
+    // if cell is clicked, the background color changes to the selected color
+    // using this keyword to call the object related to the cell style
+    if ($(this).attr('style')){
+      $(this).removeAttr('style')
+    } else {
+      $(this).attr('style', 'background-color:' + color);
+    }
+  })
+}
 
-// variable color definition
-var color;
-  // adding the click event to the color picked
-    $('td').click(function addColor(){
-      // storing the color picked
-      color = $('#colorPicker').val();
-      // if cell is clicked, the background color changes to the selected color
-      // using this keyword to call the object related to the cell style 
-      if ($(this).attr('style')){
-        $(this).removeAttr('style')
-      } else {
-        $(this).attr('style', 'background-color:' + color);
-      }
-    })
- }
 
-// variables definition for the size of the Grid
-var row, column;
 // this is the function for the Submit Query
 $('#sizePicker').submit(function (event) {
   // prevents the browser to go back to default values
   event.preventDefault();
   // storing the Grid Height value
-  row = $('#inputHeight').val();
+  const row = $('#inputHeight').val();
   // storing the Grid Width value
-  column = $('#inputWidth').val();
+  const column = $('#inputWidth').val();
 // when size is submitted by the user, call makeGrid()
   makeGrid(row, column);
 })
